0789101b5cf8453dc0ca9ef8560a95683735e426
Habibsultani <a.habibsultani@gmail.com>
2025-12-14 22:37:40 +0300

    initial comit

--- distributed-disk-register/src/main/java/com/example/family/NodeMain.java
+++ distributed-disk-register/src/main/java/com/example/family/NodeMain.java
@@ -14,6 +14,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.Socket;
 
+// this is the test commone
+
 
 import java.io.IOException;
 import java.net.ServerSocket;
