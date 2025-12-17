52fc84455514722b134aa617ead0fc509511eb37
Mucahit Bilal <bilal.kuruahmet@bil.omu.edu.tr>
2025-12-17 21:36:59 +0300

    node main'e ArrayList import edildi.

--- distributed-disk-register/src/main/java/com/example/family/NodeMain.java
+++ distributed-disk-register/src/main/java/com/example/family/NodeMain.java
@@ -7,6 +7,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
